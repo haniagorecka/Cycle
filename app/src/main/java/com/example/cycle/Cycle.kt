dcfad8c3fc5b7a4249fefd14bb000af4aa4140bf
Hanna Górecka <128626696+haniagorecka@users.noreply.github.com>
2024-04-16 18:38:12 +0200

    Update

--- app/src/main/java/com/example/cycle/Cycle.kt
+++ app/src/main/java/com/example/cycle/Cycle.kt
@@ -1,8 +1,16 @@
 package com.example.cycle
-
+/**
+ * @author Hanna Górecka
+ * Funkcja zwraca listę elementów ciągu collatza zaczynającego się od c0 przed tym jak wpadnie w cykl, przyjmuję, że cykl zaczyna się
+ * od pierwszego elementu równego 4 włącznie, więc nie wypisuję już tego elementu
+ * @param c0 pierwszy element ciągu
+ * @return lista elementów ciągu przed wpadnięciem w cykl
+ * @throws Exception jeśli pierwszy element ciągu jest niedodatni
+ */
 
 fun collatz(c0: Int): MutableList<Int>
 {
+    if(c0<=0) throw Exception ("Błędny pierwszy element ciągu")
     var list= mutableListOf<Int>()
     var c1: Int = c0
 
@@ -24,14 +32,17 @@ fun collatz(c0: Int): MutableList<Int>
 
 }
 
+/**
+ * Funkcja testująca funkcji collatz()
+ */
+fun test ()
+{
+    val list = mutableListOf<Int>(5, 16, 8)
+    assert(collatz(5)==list, {"Test funkcji zakończony niepowodzeniem"})
+    println("Test funkcji zakończony powodzeniem")
+}
+
 fun main()
 {
-    val list1 = collatz(5)
-    val list2 = collatz(4)
-    val list3 = collatz(89)
-
-    println(list1)
-    println(list2)
-    println(list3)
-
+    test()
 }
